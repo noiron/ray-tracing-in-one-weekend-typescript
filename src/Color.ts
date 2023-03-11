559cba575ddb1c41201a5df763c8238c7135a370
wu kai <wukai2048@gmail.com>
2023-03-11 14:27:50 +0800

    image-8 diffuse sphere with gamma correction

--- src/Color.ts
+++ src/Color.ts
@@ -8,11 +8,11 @@ export function writeColor(color: Color, samplesPerPixel: number) {
   let g = color.y;
   let b = color.z;
 
-  // Divide the color by the number of samples.
+  // Divide the color by the number of samples and gamma-correct for gamma=2.0.
   const scale = 1.0 / samplesPerPixel;
-  r *= scale;
-  g *= scale;
-  b *= scale;
+  r = Math.sqrt(scale * r);
+  g = Math.sqrt(scale * g);
+  b = Math.sqrt(scale * b);
 
   const ir = Math.floor(256 * clamp(r, 0.0, 0.999));
   const ig = Math.floor(256 * clamp(g, 0.0, 0.999));
